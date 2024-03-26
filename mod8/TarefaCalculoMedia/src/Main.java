b77d099d6c90412786836e689ad8324dba538609
Guildo <56208246+Guildo@users.noreply.github.com>
2024-03-26 16:11:21 -0300

    Commit do exercício do mod8

---
+++ mod8/TarefaCalculoMedia/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        calculoMedia(8, 5, 6, 10);
+    }
+
+    public static void calculoMedia (int n1, int n2, int n3, int n4) {
+        System.out.println((n1 + n2 + n3 + n4) / 4);
+    }
+}

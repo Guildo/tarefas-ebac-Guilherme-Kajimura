29055b08fb952d5cafbd41d39055bfe0c60529a3
Guildo <56208246+Guildo@users.noreply.github.com>
2024-10-09 17:02:30 -0300

    Commit do projeto do mod40

---
+++ mod40/ProjetoMod40/src/test/java/br/com/guiji/ProjetoMod40/ProjetoMod40ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guiji.ProjetoMod40;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoMod40ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

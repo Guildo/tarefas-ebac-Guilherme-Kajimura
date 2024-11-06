4f40008981d3bd9f44143644425f61da1a534b6f
Guildo <56208246+Guildo@users.noreply.github.com>
2024-11-06 15:22:17 -0300

    Adiciona novos testes

--- Recursividade/src/test/java/org/example/FibonacciTest.java
+++ Recursividade/src/test/java/org/example/FibonacciTest.java
@@ -12,4 +12,18 @@ public class FibonacciTest {
 
         assertEquals(13, result);
     }
+
+    @Test
+    public void testFibonacciOfZero() {
+        long result = Fibonacci.encontrarElementoPD(0);
+
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void testFibonacciOfOne() {
+        long result = Fibonacci.encontrarElementoPD(1);
+
+        assertEquals(1, result);
+    }
 }
